a8f431fc5d1aae2b3e0d1a266d0c8c2e69f97e56
Md Zahid Hasan <jahid1234.jh@gmail.com>
2026-05-21 10:24:06 +0600

    y

--- public/index-production.php
+++ public/index-production.php
@@ -8,6 +8,15 @@ define('LARAVEL_START', microtime(true));
 // Absolute path to Laravel app root (above public_html)
 define('APP_ROOT', '/home/abecababetisfifa/fifa2026');
 
+// Dev-build compatibility: strip /fifa2026 prefix from all requests
+// The dev build has MIX_APP_BASE=/fifa2026 baked in for API calls and routes
+if (!empty($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/fifa2026') === 0) {
+    $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen('/fifa2026')) ?: '/';
+    if (!empty($_SERVER['PATH_INFO'])) {
+        $_SERVER['PATH_INFO'] = substr($_SERVER['PATH_INFO'], strlen('/fifa2026')) ?: '/';
+    }
+}
+
 if (file_exists($maintenance = APP_ROOT.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
